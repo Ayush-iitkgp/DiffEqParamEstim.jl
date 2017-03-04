c1abaaee74a961b16274c89f5037e3a54d0ba06c
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-04 22:12:50 +0530

    Commenting each line

--- src/DiffEqParamEstim.jl
+++ src/DiffEqParamEstim.jl
@@ -26,6 +26,7 @@ using DiffEqBase, LsqFit, LossFunctions, RecursiveArrayTools, ForwardDiff, Calcu
   (f::DiffEqObjective)(x,y) = f.cost_function2(x,y)
 
 
+  # This function returns the DiffEqObjective which has 2 cost functions
   function build_loss_objective(prob::DEProblem,t,data,alg;
                                 loss_func = L2DistLoss,
                                 mpg_autodiff = false,
@@ -33,18 +34,23 @@ using DiffEqBase, LsqFit, LossFunctions, RecursiveArrayTools, ForwardDiff, Calcu
                                 verbose_steps = 100,
                                 kwargs...)
     f = prob.f
+
+    # Define first cost function
     cost_function = function (p)
       f = (t,u,du) -> prob.f(t,u,p,du)
-      uEltype = eltype(p)
-      u0 = [uEltype(prob.u0[i]) for i in 1:length(prob.u0)]
-      tspan = (uEltype(prob.tspan[1]),uEltype(prob.tspan[2]))
-      temp_prob = ODEProblem(f,u0,tspan)
+      print(f)
+      print(typeof(p))
+      uEltype = eltype(p)    # type of u = type of p
+      println(uEltype)
+      u0 = [uEltype(prob.u0[i]) for i in 1:length(prob.u0)] #Convert u0 to type of p
+      tspan = (uEltype(prob.tspan[1]),uEltype(prob.tspan[2]))  #tspan
+      temp_prob = ODEProblem(f,u0,tspan)   #ReCreat the ODE Problem
       if alg == nothing
         sol = solve(temp_prob;saveat=t,save_timeseries=false,dense=false,kwargs...)
       else
         sol = solve(temp_prob,alg;saveat=t,save_timeseries=false,dense=false,kwargs...)
       end
-      fill_length = length(t)-length(sol)
+      fill_length = length(t)-length(sol)   #length(t) = 200 length(sol) = 34
       for i in 1:fill_length
         push!(sol.u,fill(NaN,size(prob.u0)))
         #push!(sol.u,zeros(prob.u0))
@@ -53,6 +59,12 @@ using DiffEqBase, LsqFit, LossFunctions, RecursiveArrayTools, ForwardDiff, Calcu
       norm(value(loss_func(),vec(data),vec(y)))
     end
 
+    
+
+
+
+
+    # Preprocessing for second cost_function
     if mpg_autodiff
       gcfg = ForwardDiff.GradientConfig(zeros(length(f.syms)))
       g! = (x, out) -> ForwardDiff.gradient!(out, cost_function, x, gcfg)
@@ -64,6 +76,7 @@ using DiffEqBase, LsqFit, LossFunctions, RecursiveArrayTools, ForwardDiff, Calcu
       count = 0 # keep track of # function evaluations
     end
 
+    # Define second cost function
     cost_function2 = function (p,grad)
       if length(grad)>0
         g!(p,grad)
@@ -78,6 +91,7 @@ using DiffEqBase, LsqFit, LossFunctions, RecursiveArrayTools, ForwardDiff, Calcu
 
       cost_function(p)
     end
+    
     DiffEqObjective(cost_function,cost_function2)
   end
 
